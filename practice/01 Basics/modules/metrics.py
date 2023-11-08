cf5e68ffc9a36b85afa96a643c93ab76b292a877
Anastasiya Strueva <astrueva@tourmalinecore.com>
2023-11-08 14:03:57 +0500

    feat(code): add second part of "01 Basics" lab

--- practice/01 Basics/modules/metrics.py
+++ practice/01 Basics/modules/metrics.py
@@ -28,6 +28,18 @@ def ED_distance(ts1, ts2):
       ed_dist += (i-j)**2
     return ed_dist**(0.5)
 
+def redSqr(ts, mu):
+  sum = 0
+  for i in ts:
+    sum += i**2 - mu**2
+  return (sum/len(ts))**0.5
+
+def scal_prod(ts1,ts2):
+    n = len(ts1)
+    s = 0
+    for i in range(n):
+        s+= ts1[i]*ts2[i]
+    return s
 
 
 def norm_ED_distance(ts1, ts2):
@@ -47,12 +59,15 @@ def norm_ED_distance(ts1, ts2):
     norm_ed_dist : float
         The normalized Euclidean distance between ts1 and ts2.
     """
+    n = len(ts1)
+    mu1 = 1/n * sum(ts1)
+    mu2 = 1/n * sum(ts2)
+    sko1 = redSqr(ts1, mu1)
+    sko2 = redSqr(ts2, mu2)
+    ed = (np.dot(ts1,ts2)-n*mu1*mu2)/(n*sko1*sko2)
+  
+    return (abs(2*n*(1-ed)))**0.5
 
-    norm_ed_dist = 0
-
-    # INSERT YOUR CODE 
-
-    return norm_ed_dist
 
 
 def DTW_distance(ts1, ts2, r=None):
